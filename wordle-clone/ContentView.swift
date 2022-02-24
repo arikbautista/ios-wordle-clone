a9a11593b19b79747aabe10d91549b7682546b55
Arik Bautista <arik_bautista@hotmail.com>
2022-02-23 23:18:01 -0500

    Initial Commit

---
+++ wordle-clone/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  wordle-clone
+//
+//  Created by Arik Bautista on 2022-02-23.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, world!")
+            .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ wordle-clone/wordle_cloneApp.swift
@@ -0,0 +1,17 @@
+//
+//  wordle_cloneApp.swift
+//  wordle-clone
+//
+//  Created by Arik Bautista on 2022-02-23.
+//
+
+import SwiftUI
+
+@main
+struct wordle_cloneApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
