c4eb880bc242a9c2fd2f2cce740366b876641fd2
Arik Bautista <arik_bautista@hotmail.com>
2022-02-26 18:56:04 -0500

    Add basic keyboard input functionality.

--- wordle-clone/ContentView.swift
+++ wordle-clone/ContentView.swift
@@ -8,13 +8,15 @@
 import SwiftUI
 
 class BoardData: ObservableObject {
-    @Published var guessArray: [String] = ["CHORE", "CRANE"]
-    @Published var currentGuess = "Sli"
+    @Published var guessArray: [String] = []
+    @Published var currentGuess = ""
+    
+    @Published var wordLength = 5
+    @Published var numOfRows = 6
 }
 
 struct ContentView: View {
     @StateObject var globalBoardData = BoardData()
-    var numOfRows = 6
     
     var body: some View {
         VStack {
@@ -22,20 +24,20 @@ struct ContentView: View {
             BannerView()
             
             // Letter Grid
-            ForEach(0 ..< numOfRows) { rowIndex in
+            ForEach(0 ..< globalBoardData.numOfRows) { rowIndex in
                 
                 if (rowIndex == globalBoardData.guessArray.count ) {
-                    RowView(numOfLetters: 5, rowGuess: globalBoardData.currentGuess)
+                    RowView(numOfLetters: globalBoardData.wordLength, rowGuess: globalBoardData.currentGuess)
                 } else {
                     let curGuessRow = rowIndex > globalBoardData.guessArray.count
                         ? ""
                         : globalBoardData.guessArray[rowIndex]
-                    RowView(numOfLetters: 5, rowGuess: curGuessRow)
+                    RowView(numOfLetters: globalBoardData.wordLength, rowGuess: curGuessRow)
                 }
             }
             Spacer()
             
-            KeyboardView()
+            KeyboardView(boardData: globalBoardData)
             Spacer()
         }
     }
@@ -83,6 +85,8 @@ struct RowView:View {
 }
 
 struct KeyboardView:View {
+    @ObservedObject var boardData : BoardData
+    
     let topRowKeys = "QWERTYUIOP".map{String($0)}
     let middleRowKeys = "ASDFGHJKL".map{String($0)}
     let bottomRowKeys = "ZXCVBNM".map{String($0)}
@@ -92,22 +96,40 @@ struct KeyboardView:View {
     
     var body: some View {
         VStack {
-            KeyboardRow(letterArray: topRowKeys)
-            KeyboardRow(letterArray: middleRowKeys)
+            KeyboardRow(letterArray: topRowKeys, boardData: boardData)
+            KeyboardRow(letterArray: middleRowKeys, boardData: boardData)
             HStack {
-                Text(enterKeyText)
-                KeyboardRow(letterArray: bottomRowKeys)
+                
+                Text(enterKeyText).onTapGesture(perform: {
+                    let isWordFull = boardData.currentGuess.count == boardData.wordLength
+                    let isMaxGuess = boardData.guessArray.count > boardData.numOfRows
+                    
+                    if (!isWordFull || isMaxGuess) {
+                        return
+                    }
+                    
+                    boardData.guessArray.append(boardData.currentGuess)
+                    boardData.currentGuess = ""
+                })
+                
+                KeyboardRow(letterArray: bottomRowKeys, boardData: boardData)
+                
                 Text(delKeyText)
+                .onTapGesture(perform: {
+                    boardData.currentGuess = String(boardData.currentGuess.dropLast())
+                })
             }
         }
     }
 }
 
 struct KeyboardRow: View {
+    @ObservedObject var boardData : BoardData
     var letterArray: Array<String>
     
-    init(letterArray: Array<String>) {
+    init(letterArray: Array<String>, boardData: BoardData) {
         self.letterArray = letterArray
+        self.boardData = boardData
     }
     
     var body: some View {
@@ -117,7 +139,14 @@ struct KeyboardRow: View {
                     .padding(.vertical)
                     .frame(width: 24, height: 30)
                     .cornerRadius(16)
-                    .border(/*@START_MENU_TOKEN@*/Color.gray/*@END_MENU_TOKEN@*/, width: /*@START_MENU_TOKEN@*/1/*@END_MENU_TOKEN@*/)
+                    .border(Color.gray, width: /*@START_MENU_TOKEN@*/1/*@END_MENU_TOKEN@*/)
+                    .onTapGesture(perform: {
+                        if (boardData.currentGuess.count  >= boardData.wordLength) {
+                            return
+                        }
+                        
+                        boardData.currentGuess += letter
+                    })
             }
         }
     }
@@ -136,12 +165,4 @@ struct BannerView:View {
             
         }
     }
-}
-
-func updateCurrentGuess(letterToUpdate: String ) -> Void {
-    /*@ObservedObject var globalBoardData : BoardData
-    
-    if (letterToUpdate == "") {
-        globalBoardData.$currentGuess.dropLast()
-    }*/
 }
